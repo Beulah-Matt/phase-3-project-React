0939037fd9ff1fa1ccf1aeb6aad906492ca6117f
Beulah Mathenge <beulahmathenge@gmail.com>
2022-11-09 09:43:15 +0300

    Contact banner

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Home from './components/mainpage/Home';
 
 function App() {
   return (
@@ -6,6 +7,7 @@ function App() {
       <header className="App-header">
        <h1> hello world</h1>
       </header>
+      <Home />
     </div>
   );
 }
---
+++ src/components/mainpage/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ContactBanner from '../footer/ContactBanner'
+
+export default function Home() {
+  return (
+    <div>
+        <ContactBanner />
+    </div>
+  )
+}
